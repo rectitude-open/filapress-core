be0ca703525c6ea74f7b0a82fdf12c70829759fd
aspirantzhang <admin@aspirantzhang.com>
2025-05-10 17:51:33 +0800

    feat: replace php artisan test with pest in CI script

--- dev/ci.sh
+++ dev/ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-php artisan test
+/home/wwwroot/filapress-core/vendor/bin/pest
 /home/wwwroot/filapress-core/vendor/bin/pint
 /home/wwwroot/filapress-core/vendor/bin/phpstan analyse
