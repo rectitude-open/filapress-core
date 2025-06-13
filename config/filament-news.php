4dcf73be4e75fb908690c48e2c5390eedd855536
aspirantzhang <admin@aspirantzhang.com>
2025-06-13 20:31:24 +0800

    feat: update filament-news version and configs

--- config/filament-news.php
+++ config/filament-news.php
@@ -3,11 +3,30 @@
 declare(strict_types=1);
 
 return [
-    'news_model' => RectitudeOpen\FilamentNews\Models\News::class,
-    'news_category_model' => RectitudeOpen\FilamentNews\Models\NewsCategory::class,
-    'tag_model' => RectitudeOpen\FilamentNews\Models\Tag::class,
+    'cluster' => [
+        'navigation_sort' => 0,
+        'navigation_icon' => 'heroicon-o-newspaper',
+    ],
+    'news' => [
+        'navigation_sort' => 0,
+        'navigation_icon' => 'heroicon-o-newspaper',
+        'datetime_format' => 'Y-m-d H:i:s',
+        'navigation_badge' => false,
+        'model' => \RectitudeOpen\FilamentNews\Models\News::class,
+        'filament_resource' => \RectitudeOpen\FilaPressCore\Filament\Resources\NewsResource::class,
+    ],
+    'news_category' => [
+        'model' => \RectitudeOpen\FilamentNews\Models\NewsCategory::class,
+        'page' => \RectitudeOpen\FilaPressCore\Filament\Pages\NewsCategory::class,
+        'navigation_sort' => 2,
+        'navigation_icon' => 'heroicon-o-rectangle-stack',
+    ],
+    'tag' => [
+        'model' => \RectitudeOpen\FilamentNews\Models\Tag::class,
+        'filament_resource' => \RectitudeOpen\FilaPressCore\Filament\Resources\NewsTagResource::class,
+        'navigation_sort' => 3,
+        'navigation_icon' => 'heroicon-o-tag',
+    ],
 
-    'news_category_page' => RectitudeOpen\FilaPressCore\Filament\Pages\NewsCategory::class,
-    'news_filament_resource' => RectitudeOpen\FilaPressCore\Filament\Resources\NewsResource::class,
-    'news_tag_filament_resource' => RectitudeOpen\FilaPressCore\Filament\Resources\NewsTagResource::class,
+    'editor_component_class' => \RectitudeOpen\FilamentTinyEditor6\TinyEditor::class,
 ];
