03d1a18e9ecbd3772ca28c043039cea1e3144c3a
aspirantzhang <admin@aspirantzhang.com>
2025-06-20 21:55:21 +0800

    feat: update info pages config and dependencies

--- config/filament-info-pages.php
+++ config/filament-info-pages.php
@@ -8,4 +8,5 @@ return [
     'navigation_sort' => 0,
     'navigation_icon' => 'heroicon-o-document-text',
     'datetime_format' => 'Y-m-d H:i:s',
+    'editor_component_class' => \RectitudeOpen\FilamentTinyEditor6\TinyEditor::class,
 ];
