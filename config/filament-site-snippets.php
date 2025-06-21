485535baaab813dcfb62accbb7223b97f856de1d
aspirantzhang <admin@aspirantzhang.com>
2025-06-21 16:17:52 +0800

    feat: add RichEditor component class to site snippets configuration

--- config/filament-site-snippets.php
+++ config/filament-site-snippets.php
@@ -7,4 +7,5 @@ return [
     'model' => RectitudeOpen\FilamentSiteSnippets\Models\SiteSnippet::class,
     'navigation_sort' => 0,
     'navigation_icon' => 'heroicon-o-puzzle-piece',
+    'editor_component_class' => \Filament\Forms\Components\RichEditor::class,
 ];
