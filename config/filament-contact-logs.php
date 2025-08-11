f7fac691ce4f422d5ace98cdd9e2bf49443f2d44
aspirantzhang <admin@aspirantzhang.com>
2025-08-11 20:18:51 +0800

    refactor: update navigation_sort value in filament-contact-logs configuration

--- config/filament-contact-logs.php
+++ config/filament-contact-logs.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'filament_resource' => RectitudeOpen\FilamentContactLogs\Resources\ContactLogResource::class,
     'model' => RectitudeOpen\FilamentContactLogs\Models\ContactLog::class,
-    'navigation_sort' => 0,
+    'navigation_sort' => 101,
     'navigation_icon' => 'heroicon-o-envelope-open',
     'datetime_format' => 'Y-m-d H:i:s',
     'navigation_badge' => true,
